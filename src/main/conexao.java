344b69c539856bed913960d314bae1fd7be2fd0c
VictorBSz <victor.souza0350@gmail.com>
2025-05-13 16:26:13 -0300

    first commit

---
+++ src/main/conexao.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+/**
+ *
+ * @author d4rkp
+ */
+public class conexao {
+    
+    private static final String URL = "jdbc:mysql://localhost:3306/gamestore";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Rodvitvi#350";
+    
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
